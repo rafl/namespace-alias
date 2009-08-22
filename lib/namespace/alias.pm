5cc2b6e43717ed9fd94161ab52b9fe735f0c83d2
Florian Ragwitz <rafl@debian.org>
2009-08-22 15:15:51 +0200

    Add basic docs.

--- lib/namespace/alias.pm
+++ lib/namespace/alias.pm
@@ -35,3 +35,46 @@ sub import {
 }
 
 1;
+
+__END__
+
+=head1 NAME
+
+namespace::alias - Foo
+
+=head1 SYNOPSIS
+
+use namespace::alias 'My::Company::Namespace::Customer';
+
+my $cust = Customer->new;            # My::Company::Namespace::Customer->new
+my $pref = Customer::Preferred->new; # My::Company::Namespace::Customer::Preferred->new
+
+=head1 SEE ALSO
+
+=over 4
+
+=item L<aliased>
+
+=back
+
+=head1 AUTHOR
+
+Florian Ragwitz E<lt>rafl@debian.orgE<gt>
+
+With contributions from:
+
+=over 4
+
+=item Robert 'phaylon' Sedlacek E<lt>rs@474.atE<gt>
+
+=item Steffen Schwigon E<lt>ss5@renormalist.netE<gt>
+
+=back
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (c) 2009  Florian Ragwitz
+
+Licensed under the same terms as perl itself.
+
+=cut
